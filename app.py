6611950eb86f392428ccc5fa7e4b95769bc27fb1
Tharun-PS <tharun18052001@gmail.com>
2022-03-09 11:16:38 +0530

    UI

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def check():
         else:
             result.append({"Sentence": sample_text, "Status": "Unique"})
     print(result)
-    return json.dumps(json.loads(result), indent=4)
+    return json.dumps(json.loads(str(result)), indent=4)
 
 
 @app.route("/", methods=["GET", "POST"])
